29b29940575efcd7a4e097f8722a40aab3830f91
lisallreiber <lisallreiber@gmail.com>
2023-04-10 10:21:17 +0200

    dbt: add and define staging models

---
+++ berlin_bike_theft/models/staging/stg_theft_reports_archived.sql
@@ -0,0 +1,25 @@
+-- view bc in staging area we don't want to refresh tables all the time
+{{ config(materialized='view') }}
+
+select
+    -- identifiers
+    {{ dbt_utils.generate_surrogate_key(['TATZEIT_ANFANG', 'TATZEIT_ENDE', 'LOR', 'SCHADENSHOEHE']) }} as record_id,
+    cast(LOR as integer) as plr_id,
+    
+    -- timestamps
+    TATZEIT_ANFANG  as crime_period_start_datetime,
+    TATZEIT_ENDE    as crime_period_end_datetime,
+    ANGELEGT_AM     as record_created,
+    TATZEIT_DAUER   as crime_period_duration,
+    -- cast(DATE(TATZEIT_ANFANG))  as crime_period_start_date,
+    -- cast(DATE(TATZEIT_ENDE))  as crime_period_end_date,
+    -- cast(YEAR(TATZEIT_ANFANG))  as crime_period_start_year,
+    -- cast(MONTH(TATZEIT_ANFANG))  as crime_period_start_month,
+    -- cast(HOUR(TATZEIT_ANFANG))  as crime_period_start_hour,
+    -- cast(WEEKDAY(TATZEIT_ANFANG)) as crime_period_start_weekday,
+    
+    -- theft info
+    SCHADENSHOEHE as  damage_euro,
+    ART_DES_FAHRRADS as bike_type
+
+from {{ source('staging','reported_incidents_archived') }}
